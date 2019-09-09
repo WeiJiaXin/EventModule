2d9cf7cc3816d1ff07f195e815c48a9a5bf3f445
Lowy <1428416750@qq.com>
2019-09-09 08:20:45 +0800

    实现基础功能

--- Test/Test.cs
+++ Test/Test.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Lowy.Event;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    class EventData:IEvent
+    {
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        EventManager.AddListener<EventData>(CallBack1);
+        EventManager.AddListener<EventData>(CallBack2);
+        EventManager.Dispatch(new EventData());
+    }
+
+    private void CallBack2(EventData obj)
+    {
+        print("call this<button onclick=\"copyText()\">复制文本</button>");
+    }
+
+    private void CallBack1(EventData obj)
+    {
+        Call2();
+    }
+
+    private void Call2()
+    {
+        throw new System.NotImplementedException();
     }
 
     // Update is called once per frame
